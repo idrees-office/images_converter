23373beed2738a32459c52c2db1a0da4e4bdfdcc
unknown <idreesoffice@gmail.com>
2023-09-07 02:33:52 -0800

    new code

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="og:description" content="@yield('description')">
+        <meta name="og:keywords" content="@yield('keywords')">
+        <meta name="author" content="">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>@yield('title')</title>  
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>           
+        <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/css/bootstrap-icons.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/css/templatemo-topic-listing.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+        <link href="{{ asset('assets/css/uppy.css') }}" rel="stylesheet">
+
+</head>
+<body id="top">
+    @include('layouts.includes.header')
+    <main>
+        @yield('content')
+    </main>
+    @include('layouts.includes.footer')
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.sticky.js') }}"></script>
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.6.0/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+</body>
+</html>
