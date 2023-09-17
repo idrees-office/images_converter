d4e6b383ba8a20385a8d247db8977a0b9b33cbfb
idrees-office <idreesoffice9012@gmail.com>
2023-09-17 06:18:52 -0700

    new tool

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,7 +64,6 @@
                 </div>
             </div>
         </div>
-        
         <div class="row mt-4">
             <div class="col-lg-3" data-aos="zoom-in" data-aos-delay="100">
                 <div class="member d-flex align-items-start">
@@ -119,7 +118,6 @@
                 </div>
             </div>
         </div>
-        
         <div class="row mt-4">
             <div class="col-lg-3" data-aos="zoom-in" data-aos-delay="100">
                 <div class="member d-flex align-items-start">
@@ -127,8 +125,8 @@
                         <img src="{{ asset('assets/img/tools/jpg-to-png.webp') }}" class="img-fluid" alt="" />
                     </div>
                     <div class="member-info">
-                        <h4>PNG to JPG </h4>
-                        <p>Converting an image from PNG to PNG is a transformation that changes the file format while
+                        <h4>JPEG to JPG </h4>
+                        <p>Converting an image from JPEG to JPG is a transformation that changes the file format while
                             preserving the image's visual quality.</p>
                         <a href="{{ route('jpeg-to-jpg') }}" class="stretched-link"></a>
                     </div>
@@ -140,8 +138,8 @@
                     </div>
 
                     <div class="member-info">
-                        <h4>PNG to JPEG</h4>
-                        <p>Converting an image from PNG to JPEG is a transformation that changes the file format while
+                        <h4>JPEG to PNG</h4>
+                        <p>Converting an image from JPEG to PNG is a transformation that changes the file format while
                             preserving the image's visual quality.</p>
                         <a href="{{ route('jpeg-to-png') }}" class="stretched-link"></a>
                     </div>
@@ -151,10 +149,9 @@
                 <div class="member d-flex align-items-start">
                     <div class="pic"><img src="{{ asset('assets/img/tools/jpg-to-png.webp') }}" class="img-fluid" alt="" />
                     </div>
-
                     <div class="member-info">
-                        <h4>PNG to WEBP</h4>
-                        <p>Converting an image from PNG to WEBP is a transformation that changes the file format while
+                        <h4> JPEG to WEBP</h4>
+                        <p>Converting an image from JPEG to WEBP is a transformation that changes the file format while
                             preserving the image's visual quality.</p>
                         <a href="{{ route('jpeg-to-webp') }}" class="stretched-link"></a>
                     </div>
@@ -166,8 +163,8 @@
                     </div>
 
                     <div class="member-info">
-                        <h4>PNG to HEIC</h4>
-                        <p>Converting an image from PNG to HEIC is a transformation that changes the file format while
+                        <h4>JPEG to HEIC</h4>
+                        <p>Converting an image from JPEG to HEIC is a transformation that changes the file format while
                             preserving the image's visual quality.</p>
                         <a href="{{ route('jpeg-to-heic') }}" class="stretched-link"></a>
                     </div>
@@ -184,7 +181,7 @@
         </div>
     </div>
     </div>
-    <div class="container-fluid">
+    {{-- <div class="container-fluid">
         <div class="row">
             <ul class="nav nav-tabs" id="myTab" role="tablist">
                 <li class="nav-item" role="presentation">
@@ -218,8 +215,7 @@
                 </li>
             </ul>
         </div>
-    </div>
-
+    </div> --}}
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -229,10 +225,29 @@
                         <div class="row">
                             <div class="col-lg-3 col-md-3 col-12 mb-4 mb-lg-3">
                                 <div class="custom-block bg-white shadow-lg">
-                                    <a href="{{ route('jpg-to-png') }}">
+                                    <a href="#">
+                                        <div class="d-flex">
+                                            <div> <h5 class="mb-2">Compress IMAGE</h5> </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-12">
+                                <div class="custom-block bg-white shadow-lg">
+                                    <a href="#">
                                         <div class="d-flex">
                                             <div>
-                                                <h5 class="mb-2">JPG To PNG</h5>
+                                                <h5 class="mb-2">Resize IMAGE</h5>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-12">
+                                <div class="custom-block bg-white shadow-lg">
+                                    <a href="topics-detail.html">
+                                        <div class="d-flex">
+                                            <div> <h5 class="mb-2">Crop IMAGE</h5>
                                             </div>
                                         </div>
                                     </a>
@@ -243,28 +258,7 @@
                                     <a href="topics-detail.html">
                                         <div class="d-flex">
                                             <div>
-                                                <h5 class="mb-2">JPG To WEBP</h5>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-md-3 col-12">
-                                <div class="custom-block bg-white shadow-lg">
-                                    <a href="topics-detail.html">
-                                        <div class="d-flex">
-                                            <div> <h5 class="mb-2">JPG To JPEG</h5>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-md-3 col-12">
-                                <div class="custom-block bg-white shadow-lg">
-                                    <a href="topics-detail.html">
-                                        <div class="d-flex">
-                                            <div>
-                                                <h5 class="mb-2">JPG To HEIC</h5>
+                                                <h5 class="mb-2">Rotate IMAGE</h5>
                                             </div>
                                         </div>
                                     </a>
@@ -378,10 +372,8 @@
                             </div>
                         </div>
                     </div>
-
                     <div class="tab-pane fade" id="music-tab-pane" role="tabpanel" aria-labelledby="music-tab"
                         tabindex="0">
-
                         <div class="row">
                             <div class="col-lg-3 col-md-3 col-12 mb-4 mb-lg-3">
                                 <div class="custom-block bg-white shadow-lg">
@@ -564,11 +556,9 @@
                         </div>
 
                         <li>
-                            <h4 class="text-white mb-3">Search your favourite topic</h4>
+                            <h4 class="text-white mb-3">Select a Tool or Software</h4>
 
-                            <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-                                cumque magnam? Sequi, cupiditate quibusdam alias illum sed esse ad dignissimos libero
-                                sunt, quisquam numquam aliquam? Voluptas, accusamus omnis?</p>
+                            <p class="text-white">There are numerous software applications, online tools, and programming libraries available that can perform this conversion. Some common options include</p>
 
                             <div class="icon-holder">
                                 <i class="bi-search"></i>
@@ -576,11 +566,9 @@
                         </li>
 
                         <li>
-                            <h4 class="text-white mb-3">Bookmark &amp; Keep it for yourself</h4>
+                            <h4 class="text-white mb-3">Load &amp; or Upload the JPG Image</h4>
 
-                            <p class="text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint animi
-                                necessitatibus aperiam repudiandae nam omnis est vel quo, nihil repellat quia velit
-                                error modi earum similique odit labore. Doloremque, repudiandae?</p>
+                            <p class="text-white">Depending on the tool you're using, you'll either open the JPG file in the software or upload it to the online converter.</p>
 
                             <div class="icon-holder">
                                 <i class="bi-bookmark"></i>
@@ -588,11 +576,8 @@
                         </li>
 
                         <li>
-                            <h4 class="text-white mb-3">Read &amp; Enjoy</h4>
-
-                            <p class="text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi vero
-                                quisquam, rem assumenda similique voluptas distinctio, iste est hic eveniet debitis ut
-                                ducimus beatae id? Quam culpa deleniti officiis autem?</p>
+                            <h4 class="text-white mb-3">Click the Upload &amp; Enjoy</h4>
+                            <p class="text-white">Click the "Upload" button, and then wait for your images to be downloaded in a zip file after 2 minutes</p>
 
                             <div class="icon-holder">
                                 <i class="bi-book"></i>
@@ -602,12 +587,12 @@
                 </div>
             </div>
 
-            <div class="col-12 text-center mt-5">
+            {{-- <div class="col-12 text-center mt-5">
                 <p class="text-white">
                     Want to learn more?
                     <a href="#" class="btn custom-btn custom-border-btn ms-3">Check out Youtube</a>
                 </p>
-            </div>
+            </div> --}}
         </div>
     </div>
 </section>
@@ -625,69 +610,57 @@
             <div class="col-lg-5 col-12">
                 <img src="{{ asset('assets/images/faq_graphic.jpg')}}" class="img-fluid" alt="FAQs">
             </div>
-
             <div class="col-lg-6 col-12 m-auto">
                 <div class="accordion" id="accordionExample">
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingOne">
                             <button class="accordion-button" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                What is Topic Listing?
+                                What is an image converter ?
                             </button>
                         </h2>
 
                         <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
                             data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                Topic Listing is free Bootstrap 5 CSS template. <strong>You are not allowed to
-                                    redistribute this template</strong> on any other template collection website without
-                                our permission. Please contact TemplateMo for more detail. Thank you.
+                                An image converter is a tool or software that changes the format of an image from one type to another, such as from JPG to PNG.
                             </div>
                         </div>
                     </div>
-
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingTwo">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                How to find a topic?
+                                How do I use an online image converter?
                             </button>
                         </h2>
-
                         <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
                             data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                You can search on Google with <strong>keywords</strong> such as templatemo portfolio,
-                                templatemo one-page layouts, photography, digital marketing, etc.
+                                To use an online image converter, visit a conversion website, upload your image, select the desired format, and click "Convert." Download the converted image when it's ready.
                             </div>
                         </div>
                     </div>
-
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingThree">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                Does it need to paid?
+                                Why might I need to convert images?
                             </button>
                         </h2>
-
                         <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
                             data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                You can modify any of this with custom CSS or overriding our default variables. It's
-                                also worth noting that just about any HTML can go within the
-                                <code>.accordion-body</code>, though the transition does limit overflow.
+                                A3: Image conversion is useful for various purposes, including compatibility with different devices or software, reducing file size, or changing an image's characteristics, such as transparency.
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 </section>
-
-<section class="contact-section section-padding section-bg" id="section_5">
+{{-- <section class="contact-section section-padding section-bg" id="section_5">
     <div class="container">
         <div class="row">
 
@@ -696,20 +669,20 @@
             </div>
 
             <div class="col-lg-5 col-12 mb-4 mb-lg-0">
-                <iframe class="google-map"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2595.065641062665!2d-122.4230416990949!3d37.80335401520422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858127459fabad%3A0x808ba520e5e9edb7!2sFrancisco%20Park!5e1!3m2!1sen!2sth!4v1684340239744!5m2!1sen!2sth"
-                    width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14433.037165016929!2d55.32541979999999!3d25.2618616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sae!4v1694955395845!5m2!1sen!2sae" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
             </div>
 
-            <div class="col-lg-3 col-md-6 col-12 mb-3 mb-lg- mb-md-0 ms-auto">
-                <h4 class="mb-3">Head office</h4>
+            <div class="col-lg-3 col-md-6 col-12 mb-3 mb-lg- mb-md-0 ms-auto mt-5">
+                <h4 class="mb-3">Lhore office</h4>
                 <p>Bay St &amp; johar town Lahore</p>
                 <hr>
                 <p class="d-flex align-items-center mb-1">
                     <span class="me-2">Phone</span>
-                    <a href="tel: 305-240-9671" class="site-footer-link">
+                    <a href="tel:+923237077" class="site-footer-link">
                         305-240-9671
+                        
                     </a>
                 </p>
                 <p class="d-flex align-items-center">
@@ -720,7 +693,7 @@
                 </p>
             </div>
 
-            <div class="col-lg-3 col-md-6 col-12 mx-auto">
+            <div class="col-lg-3 col-md-6 col-12 mx-auto mt-5">
                 <h4 class="mb-3">Dubai office</h4>
                 <p>Deria Dubai</p>
                 <hr>
@@ -736,7 +709,7 @@
 
         </div>
     </div>
-</section>
+</section> --}}
 
 
 
